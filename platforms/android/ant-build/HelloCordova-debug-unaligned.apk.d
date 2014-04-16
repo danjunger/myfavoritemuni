d3c33d97749e73d4e4b7901839d6e451554da102
danjunger <junger.da@gmail.com>
2014-04-15 17:55:17 -0700

    add auto refresh

--- platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\Users\junge_000\Documents\GitHub\ionictest2\platforms\android\ant-build\HelloCordova-debug-unaligned.apk : \
-C:\Users\junge_000\Documents\GitHub\ionictest2\platforms\android\ant-build\HelloCordova.ap_ \
-C:\Users\junge_000\Documents\GitHub\ionictest2\platforms\android\ant-build\classes.dex \
-C:\Users\junge_000\Documents\GitHub\ionictest2\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\junge_000\Documents\GitHub\myfavoritemuni-ionic\platforms\android\ant-build\HelloCordova-debug-unaligned.apk : \
+C:\Users\junge_000\Documents\GitHub\myfavoritemuni-ionic\platforms\android\ant-build\HelloCordova.ap_ \
+C:\Users\junge_000\Documents\GitHub\myfavoritemuni-ionic\platforms\android\ant-build\classes.dex \
+C:\Users\junge_000\Documents\GitHub\myfavoritemuni-ionic\platforms\android\CordovaLib\ant-build\classes.jar \
