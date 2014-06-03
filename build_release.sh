2e8e7435873adf501a658f710670bcc6c1acce94
Daniel Junger <junger.da@gmail.com>
2014-06-03 15:26:52 -0700

    cleanup repo

--- build_release.sh
+++
@@ -1,3 +0,0 @@
-cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release-key.keystore platforms/android/ant-build/MyFavoriteMuni-release-unsigned.apk release_key
-zipalign -v 4 platforms/android/ant-build/MyFavoriteMuni-release-unsigned.apk MyFavoriteMuni.apk
--- update_manifest.sh
+++
@@ -1,4 +0,0 @@
-function fix-android-manifest { 
-local versionCode=$(sed -n '/versionCode/s/.*versionCode[^"]*"\([0-9]*\).*/\1/p' platforms/android/assets/www/config.xml); 
-sed -i '' -e 's/versionCode="[0-9]*"/versionCode="'$versionCode'"/' platforms/android/AndroidManifest.xml; 
-} 
